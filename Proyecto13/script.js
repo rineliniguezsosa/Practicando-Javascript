f1ed30e8892f459749437774c789425c3028b05c
rinel <rineliniguezsosa@gmail.com>
2021-07-14 21:36:17 -0500

    fase 2 proyecto13

--- Proyecto13/script.js
+++ Proyecto13/script.js
@@ -1,10 +1,45 @@
-window.onload = function(){
+window.addEventListener("load",()=>{
+  
+   document.getElementById("cinco").addEventListener("click",(e)=>{
+      Proceso(e.target.value)
+   })
+   document.getElementById("diez").addEventListener("click",(e)=>{
+      Proceso(e.target.value)
+   })
+   document.getElementById("quince").addEventListener("click",(e)=>{
+      Proceso(e.target.value)
+   })
+   document.getElementById("vcinco").addEventListener("click",(e)=>{
+      Proceso(e.target.value)
+   })
+   document.getElementById("cincuenta").addEventListener("click",(e)=>{
+      Proceso(e.target.value)
+   })
+    
+  const Proceso = (num) => {
+     Clock(num)
+  }
+  const Clock = (per) => {
+   let cantidadtotal = document.getElementById("cantidadtotal").value
+   let cantidadtotalcopy = parseFloat(cantidadtotal)
+   let percopy = parseFloat(per)
+   let personas = document.getElementById("personas").value
+   let personascopy = parseInt(personas)
+   let result = (cantidadtotalcopy*percopy)/personascopy
+   console.log(result.toFixed(2));
+   //console.log(typeof personascopy);
+   //console.log(typeof percopy);
+   //console.log(typeof cantidadtotalcopy);//number
+   totaltip.textContent = result.toString()
+  }
+  let time = setInterval(Clock, 1000)
+  
+  clearInterval(time)
+  
+  
+  
+ 
+})
 
-  let monto = 142.55
-  let porcentaje = 0.15
 
-  let montotal = monto * (1+porcentaje)
-  let cantidadxpersona = (montotal/5)
-  console.log(cantidadxpersona.toFixed(2))
 
-}
