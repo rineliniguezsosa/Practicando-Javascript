e3f699f2c31cf02b8d8f048dc0c516ee94a9b6e5
rinel <rineliniguezsosa@gmail.com>
2021-07-22 21:44:21 -0500

    fase 1 calculadora

--- Proyecto14/script.js
+++ Proyecto14/script.js
@@ -1,12 +1,56 @@
 window.addEventListener("load",()=>{
-   let data
-    function Sum(params) {
-       return params +1
-    }
 
-    data = Sum(7)
-    console.log(data);
-   
+  let ocho = document.getElementById("eight");
+  let seven = document.getElementById("seven");
+  let nine = document.getElementById("nine");
+  let dot = document.getElementById("dot");
+  let display = document.getElementById("display");
+  let mas = document.getElementById("mas");
+  let menos = document.getElementById("menos");
+  let eliminar = document.querySelector(".eliminar")
+  let misdatos = [];
+  
+  setInterval(() => {
+    console.log(misdatos);
+  }, 1000)
+  
+  
+  eliminar.addEventListener("click",()=>{
+    display.value = ""
+  })
+  ocho.addEventListener("click",(e)=>{
+    misdatos.push(e.target.value) 
+  })
+  seven.addEventListener("click",(e)=>{
+    misdatos.push(e.target.value)
+  })
+  nine.addEventListener("click",(e)=>{
+    misdatos.push(e.target.value)
+  })
+  dot.addEventListener("click",(e)=>{
+    misdatos.push(e.target.value)
+  })
+  menos.addEventListener("click",(e)=>{
+    misdatos.push(e.target.value)
+  })
+  mas.addEventListener("click",(e)=>{
+    misdatos.push(e.target.value)
+  })
+  
+  
+  
+  
+  
+
+  
+  
+  
+
+
+  
+ 
+
+
 })
 
 
