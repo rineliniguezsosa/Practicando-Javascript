c594ff06ab3d855efee5124686aaf6c28bebb4fe
rinel <rineliniguezsosa@gmail.com>
2021-08-11 13:18:35 -0500

    proyecto 16 preguntas

--- Proyecto16/script.js
+++ Proyecto16/script.js
@@ -1,6 +1,23 @@
 window.addEventListener("load",()=>{
   
+  let form = document.querySelector(".form");
+  //let nameuser = document.querySelector(".nameuser").value;
   
+
+  form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    window.location.href = "Question1.html"
+  })
+  
+  
+  
+  
+  
+  
+
+  
+
+
    
   
 })
